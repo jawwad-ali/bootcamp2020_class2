b26f41d75c6396143fc4c40cf9447b8ae496ca6c
aashirasif <71927384+aashirasif@users.noreply.github.com>
2020-09-29 09:10:09 +0500

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div class="Main">
+      <div class="Main2">
+        <ul>
+          <li>Aashir</li>
+          <li>Aashir</li>
+          <li>Aashir</li>
+          <li>Aashir</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default App;
